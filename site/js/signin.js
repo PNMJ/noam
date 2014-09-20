9d5e8b0bcb4a194e77d35b01b85b16d882caa536
ducknoise <noaml1295@gmail.com>
2014-09-20 12:30:43 -0400

    gah

--- site/js/signin.js
+++ site/js/signin.js
@@ -1,7 +1,6 @@
 $(document).ready(function() {
 	$("#signupbut").click(function(event) {
-		var gah = $("#sign-up-form").serialize();
+		var gah = $("#sign-in-form").serialize();
 		console.log(gah);
-		alert("Sign Up Succesful!");
 	});
 });
