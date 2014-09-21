2fb1f4f2e1ed1a1227956fdee34cf3a24c427702
ducknoise <noaml1295@gmail.com>
2014-09-20 22:23:54 -0400

    Put me back on the line

--- site/js/home.js
+++ site/js/home.js
@@ -1,7 +1,25 @@
 $(document).ready(function() {
 	$("#img-holder").height($("#img-holder").width());
 	
-	$("#slidedisbitch").click(function(event) {
-		$("#slide1").slide();
+	$(".slideForward").click(function(event) {
+		nextSlide();
 	});
+	$(".slideBack").click(function(event) {
+		prevSlide();
+	});
+
 });
+var onslide = 1;
+var slideID = "#slide1"
+function nextSlide(){
+	$(slideID).hide('slide', {direction: 'left'}, 200);
+	onslide++;
+	slideID = "#slide" + onslide;
+	$(slideID).delay(300).show('slide', {direction: 'right'}, 300);
+}
+function prevSlide(){
+	$(slideID).hide('slide', {direction: 'right'}, 200);
+	onslide--;
+	slideID = "#slide" + onslide;
+	$(slideID).delay(300).show('slide', {direction: 'left'}, 300);
+}
